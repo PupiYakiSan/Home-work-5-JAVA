4fe4bff8e355ce6f313583f16d17367f0cae51d9
Olga <o.n.p@mail.ru>
2023-04-28 09:08:11 +0300

    Modification in Parameterized Test

--- src/test/java/ru/netology/services/RestMonthServiceTest.java
+++ src/test/java/ru/netology/services/RestMonthServiceTest.java
@@ -1,26 +1,18 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestMonthServiceTest {
 
-    @Test
-    public void test1 (){
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/income and expenses.csv")
+
+    public void testRest (int expected, int income, int expenses, int threshold){
         RestMonthService service = new RestMonthService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test2 (){
-        RestMonthService service = new RestMonthService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
